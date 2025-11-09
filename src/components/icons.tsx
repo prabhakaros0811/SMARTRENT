6fdc7764327f99b8e66ddae6623a6c88f203d1f2
25MX339 - PRABHAKAR O S <25mx339@psgtech.ac.in>
2025-11-09 05:09:04 +0000

    can you give me new besy logo (_for element div_)

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -13,8 +13,11 @@ export const Icons = {
       strokeLinejoin="round"
       {...props}
     >
-      <path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z" />
-      <polyline points="9 22 9 12 15 12 15 22" />
+      <path d="M12 2L2 12h3v10h14V12h3L12 2z" />
+      <path d="M12 10v12" />
+      <path d="M16 16h-8" />
+      <path d="M16 22h-8" />
+      <path d="M16 10h-8" />
     </svg>
   ),
   owner: (props: LucideProps) => <UserCog {...props} className="text-primary"/>,
